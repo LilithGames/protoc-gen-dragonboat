9dedc9efc47662bbe2affa725bab3f129cc261ad
hulucc <hulucc@outlook.com>
2022-10-25 17:34:27 +0800

    feat: support proto3 optional

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"google.golang.org/protobuf/types/pluginpb"
 	pgs "github.com/lyft/protoc-gen-star"
 	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
 )
 
 func main() {
-	g := pgs.Init()
+	var supportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+	g := pgs.Init(pgs.SupportedFeatures(&supportedFeatures))
 	g.RegisterModule(NewDragonboat())
 	g.RegisterPostProcessor(pgsgo.GoFmt())
 	g.Render()
